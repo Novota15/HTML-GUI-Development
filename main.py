9d92fe40979790a5a5f06f530df3623e83814cfe
Grant Novota <47872713+Novota15@users.noreply.github.com>
2022-01-08 16:25:05 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,11 @@ cwd = os.getcwd() # used by static file server
 class BaseHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
         print("setting headers!!!")
-        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("access-control-allow-origin", "*")
         self.set_header("Access-Control-Allow-Headers", "x-requested-with")
-        self.set_header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
+        self.set_header('Access-Control-Allow-Methods', 'GET, PUT, DELETE, OPTIONS')
+        # HEADERS!
+        self.set_header("Access-Control-Allow-Headers", "access-control-allow-origin,authorization,content-type")
 
     def options(self):
         # no body
