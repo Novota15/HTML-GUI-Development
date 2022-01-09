34482ce81ece463beb8b856be1608ec4e5940184
Grant Novota <47872713+Novota15@users.noreply.github.com>
2022-01-08 18:28:49 -0700

    render graph

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import tornado.ioloop
 import tornado.web
 import os
 import json
+import matplotlib.pyplot as plt
+import base64
+from io import BytesIO
 
 # my libraries
 import db
@@ -56,6 +59,22 @@ class CommandHandler(BaseHandler):
     
     # handle both GET and POST requests with the same function
     def handleRequest(self):
+        
+
+        fig = plt.figure()
+        #plot sth
+
+        tmpfile = BytesIO()
+        fig.savefig(tmpfile, format='png')
+        encoded = base64.b64encode(tmpfile.getvalue()).decode('utf-8')
+
+        html = 'Some html head' + '<img src=\'data:image/png;base64,{}\'>'.format(encoded) + 'Some more html'
+
+        with open('test.html','w') as f:
+            f.write(html)
+
+        self.render('index.html')
+
         # is op to decide what kind of command is being sent
         op = self.get_argument('op',None)
         
