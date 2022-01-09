e1f0d95df9d994a83c7ebe6c9f3bb22451052cb7
Grant Novota <47872713+Novota15@users.noreply.github.com>
2022-01-09 15:38:27 -0700

    create alarms

--- main.py
+++ main.py
@@ -26,6 +26,12 @@ temp_max_limit = 80.0
 humid_min_limit = 30.0
 humid_max_limit = 70.0
 
+# alarms
+temp_min_alarm = False
+temp_max_alarm = False
+humid_min_alarm = False
+humid_max_alarm = False
+
 # allow cross-origin requests
 class BaseHandler(tornado.web.RequestHandler):
     def set_default_headers(self):
@@ -64,12 +70,14 @@ class CommandHandler(BaseHandler):
         # is op to decide what kind of command is being sent
         op = self.get_argument('op', None)
 
-        
+        global temp_min_limit, temp_max_limit, humid_min_limit, humid_max_limit
+        global temp_min_alarm, temp_max_alarm, humid_min_alarm, humid_max_alarm
+
         #received a "checkup" operation command from the browser:
         if op == "checkup":
             print("checkup called")
             #make a dictionary
-            status = {"server": True, "mostRecentSerial": "success" }
+            status = {"server": True }
             #turn it to JSON and send it to the browser
             self.write( json.dumps(status) )
 
@@ -78,7 +86,9 @@ class CommandHandler(BaseHandler):
             single_sample()
             #make a dictionary
             global current_temp, current_humidity
-            status = {"server": True, "current_temp": current_temp, "current_humidity": current_humidity }
+            status = {"server": True, "current_temp": current_temp, "current_humidity": current_humidity,
+                "temp_max_alarm": temp_max_alarm, "humid_max_alarm": humid_max_alarm,
+                "temp_min_alarm": temp_min_alarm, "humid_min_alarm": humid_min_alarm}
             #turn it to JSON and send it to the browser
             self.write( json.dumps(status) )
 
@@ -91,7 +101,9 @@ class CommandHandler(BaseHandler):
                 single_sample()
                 time.sleep(1)
             global current_temp, current_humidity
-            status = {"server": True, "current_temp": current_humidity, "current_humidity": current_humidity }
+            status = {"server": True, "current_temp": current_temp, "current_humidity": current_humidity,
+                "temp_max_alarm": temp_max_alarm, "humid_max_alarm": humid_max_alarm,
+                "temp_min_alarm": temp_min_alarm, "humid_min_alarm": humid_min_alarm}
             #turn it to JSON and send it to the browser
             self.write( json.dumps(status) )
 
@@ -105,18 +117,22 @@ class CommandHandler(BaseHandler):
 
         elif op == "set max temp":
             value = self.get_argument('value', None)
+            temp_max_limit = float(value)
             print("max temp value:", value)
 
         elif op == "set max humidity":
             value = self.get_argument('value', None)
+            humid_max_limit = float(value)
             print("max humidity value:", value)
 
         elif op == "set min temp":
             value = self.get_argument('value', None)
+            temp_min_limit = float(value)
             print("min temp value:", value)
 
         elif op == "set min humidity":
             value = self.get_argument('value', None)
+            humid_min_limit = float(value)
             print("min humidity value:", value)
 
         elif op == "stop server":
@@ -157,6 +173,25 @@ def sample_data():
     now = datetime.now()
     db.add_temp(session, temp_f, temp_c, now)
     db.add_humidity(session, h, now)
+
+    # check if we hit an alarm
+    global temp_min_limit, temp_max_limit, humid_min_limit, humid_max_limit
+    global temp_min_alarm, temp_max_alarm, humid_min_alarm, humid_max_alarm
+
+    # reset alarms
+    temp_min_alarm = False
+    temp_max_alarm = False
+    humid_min_alarm = False
+    humid_max_alarm = False
+    
+    if temp_f > temp_max_limit:
+        temp_max_alarm = True
+    elif temp_f < temp_min_limit:
+        temp_min_alarm = True
+    elif h > humid_max_limit:
+        humid_max_alarm = True
+    elif h < humid_min_limit:
+        humid_min_alarm = True
     return h, temp_f
 
 def single_sample():
